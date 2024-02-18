063612c094078c83da4896a703c932fa999ae315
Alan Smith <95879019+alan-d-smith@users.noreply.github.com>
2024-02-18 23:27:36 +0000

    fix(ccc): correct 2015 S1 approach after misreading the problem

--- problems/ccc/ccc_2015-s1-zero-that-out.java
+++ problems/ccc/ccc_2015-s1-zero-that-out.java
@@ -63,12 +63,12 @@ class S1_Zero_That_Out extends Challenge {
     public static void main(String[] args) {
         Stack<Integer> inputStack = new Stack<>();
         Scanner scanner = new Scanner(System.in);
-        String current_raw = scanner.nextLine();
+        int number_of_lines = scanner.nextInt();
         int sum = 0;
 
-        while (!current_raw.isEmpty())
+        for (int i=0; i<number_of_lines; i++)
         {
-            int current = Integer.parseInt(current_raw);
+            int current = scanner.nextInt();
 
             if (current == 0)
             {
@@ -83,7 +83,6 @@ class S1_Zero_That_Out extends Challenge {
                 //System.out.println("Added:" + current);
             }
             //System.out.println("Current Sum:" + sum);
-            current_raw = scanner.nextLine();
         }
 
         // Final sum
